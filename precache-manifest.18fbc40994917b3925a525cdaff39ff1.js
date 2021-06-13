4ca728d5d63d00d9c101c2b9a10825ad6fd28e39
luckybyc <luckybyc@users.noreply.github.com>
2021-06-13 22:32:13 +0000

    added express and Edited Package ba7962dcba6de37c784f366a71ae6f51cc4397e7

---
+++ precache-manifest.18fbc40994917b3925a525cdaff39ff1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2319abd1fcc3a73c245015355f95d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0006819d05fd97d625ab",
+    "url": "/static/css/main.01b0c21b.chunk.css"
+  },
+  {
+    "revision": "64d3a846973ba78f8784",
+    "url": "/static/js/2.55ab6ccb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.55ab6ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0006819d05fd97d625ab",
+    "url": "/static/js/main.d9db9f65.chunk.js"
+  },
+  {
+    "revision": "8cb4ec54fa2007826be9",
+    "url": "/static/js/runtime-main.05172a96.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "1b38414956c666bd1df78fe5b9c84756",
+    "url": "/static/media/Montserrat-BoldItalic.1b384149.ttf"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "/static/media/Montserrat-Italic.a7063e0c.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "40a74702035bf9ef19053c84ce9a58b9",
+    "url": "/static/media/Montserrat-MediumItalic.40a74702.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "50f13391babe5b650b8a0589dd962f74",
+    "url": "/static/media/facebook.50f13391.svg"
+  },
+  {
+    "revision": "2234448da3ffed7f7d4a818070def85d",
+    "url": "/static/media/instagram.2234448d.svg"
+  },
+  {
+    "revision": "b51e2abd118874c5f4641dc547551a79",
+    "url": "/static/media/sahajomy.b51e2abd.png"
+  },
+  {
+    "revision": "a50716dd845ea43407a9a1bcffe42aef",
+    "url": "/static/media/twitter.a50716dd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coming-soon/precache-manifest.637eb7b7d0d87fe169a2d97b0360b33a.js"
+  "/precache-manifest.18fbc40994917b3925a525cdaff39ff1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coming-soon/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
